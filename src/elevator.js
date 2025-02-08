6381bab37da12eea974f72a35ae8d4d9cdccda23
Rocco Barbini <rocco_tech@proton.me>
2025-02-08 22:30:45 +0100

    second challenge won

--- src/elevator.js
+++ src/elevator.js
@@ -3,6 +3,40 @@
         var elevator = elevators[0]; // Let's use the first elevator
         var max_floor = floors.length - 1;
         var mid_floor = Math.floor(max_floor / 2);
+        elevator.unqueued_requests = { // requests not in the direction of travel, to be queued later
+            'queue' : [],
+            'add' : function(floorNum) {
+                if( !this.queue.includes(floorNum) ) {
+                    this.queue.push(floorNum);
+                }
+            },
+            'get' : function(direction) {
+                let requests = [];
+                switch (direction) {
+                    case "up":
+                        requests = this.queue.sort((a, b) => a - b);
+                        this.queue = [];
+                        break;
+                    case "down":
+                        requests = this.queue.sort((a, b) => b - a);
+                        this.queue = [];
+                        break;
+                }
+                return requests;
+            }
+        }
+        let same_direction = function(elevator, floorNum) {
+            if(
+                (elevator.currentFloor < floorNum && elevator.destinationDirection() == "up") ||
+                (elevator.currentFloor > floorNum && elevator.destinationDirection() == "down") ||
+                elevator.destinationDirection() == "stopped"
+            ) {
+                // the floor is in the direction of travel
+                return true;
+            }
+            // the floor is not in the direction of travel
+            return false;
+        }
 
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", function() {
@@ -11,19 +45,86 @@
         });
         elevator.on("floor_button_pressed", function(floorNum) {
             console.log(floorNum + " floor button presses");
-            elevator.goToFloor(floorNum);
+            if (!elevator.destinationQueue.includes(floorNum)) {
+                if (same_direction(elevator, floorNum)) {
+                    console.log(floorNum + " added to the queue");
+                    elevator.goToFloor(floorNum);
+                    switch(elevator.destinationDirection()) {
+                        case "up":
+                            elevator.destinationQueue.sort((a, b) => a - b);
+                            break;
+                        case "down":
+                            elevator.destinationQueue.sort((a, b) => b - a);
+                            break;
+                        case "stopped":
+                            if (floorNum >= mid_floor) {
+                                elevator.destinationQueue.sort((a, b) => b - a);
+                            } else {
+                                elevator.destinationQueue.sort((a, b) => a - b);
+                            }
+                    }
+                    elevator.checkDestinationQueue();
+                }
+            } else {
+                elevator.unqueued_requests.add(floorNum);
+            }
         })
 
         for (let i = 0; i < floors.length; i++) {
             floors[i].on("up_button_pressed", function() {
-                // will improve it later by checking the direction of travel to optimise
+                // enqueue now if on direction of travel
                 console.log("up button pressed at floor " + floors[i].floorNum());
+                if (!elevator.destinationQueue.includes(floors[i].floorNum())) {
+                    if (same_direction(elevator, floors[i].floorNum())) {
+                        console.log(floors[i].floorNum() + " added to the queue");
+                        elevator.goToFloor(floors[i].floorNum());
+                        switch(elevator.destinationDirection()) {
+                            case "up":
+                                elevator.destinationQueue.sort((a, b) => a - b);
+                                break;
+                            case "down":
+                                elevator.destinationQueue.sort((a, b) => b - a);
+                                break;
+                            case "stopped":
+                                if (floors[i].floorNum() >= mid_floor) {
+                                    elevator.destinationQueue.sort((a, b) => b - a);
+                                } else {
+                                    elevator.destinationQueue.sort((a, b) => a - b);
+                                }
+                        }
+                        elevator.checkDestinationQueue();
+                    }
+                } else {
+                    elevator.unqueued_requests.add(floors[i].floorNum());
+                }
                 elevators[0].goToFloor(floors[i].floorNum());
             });
             floors[i].on("down_button_pressed", function() {
-                // will improve it later by checking the direction of travel to optimise
+                // enqueue now if on direction of travel
                 console.log("down button pressed at floor " + floors[i].floorNum());
-                elevators[0].goToFloor(floors[i].floorNum());
+                if (!elevator.destinationQueue.includes(floors[i].floorNum())) {
+                    if (same_direction(elevator, floors[i].floorNum())) {
+                        console.log(floors[i].floorNum() + " added to the queue");
+                        elevator.goToFloor(floors[i].floorNum());
+                        switch(elevator.destinationDirection()) {
+                            case "up":
+                                elevator.destinationQueue.sort((a, b) => a - b);
+                                break;
+                            case "down":
+                                elevator.destinationQueue.sort((a, b) => b - a);
+                                break;
+                            case "stopped":
+                                if (floors[i].floorNum() >= mid_floor) {
+                                    elevator.destinationQueue.sort((a, b) => b - a);
+                                } else {
+                                    elevator.destinationQueue.sort((a, b) => a - b);
+                                }
+                        }
+                        elevator.checkDestinationQueue();
+                    }
+                } else {
+                    elevator.unqueued_requests.add(floors[i].floorNum());
+                }
             });
         }
     },
